e69925a212e0b3bcd960f8bd61e303e6e6dc36ed
Patrícia Santana <patriciasan3@gmail.com>
2021-11-12 16:16:44 -0300

    mudanças na function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
-function idade() {
-    var idade = document.querySelector('input#idade')
-        if (idade < 18)
-        console.log('')
+function best() {
+    //var idade = 22
+    //var age = document.getElementById('idade')
+    var age = Number(idade.value)
+    //var better = document.getElementById('')
+        if (age > 0 && age < 18) {
+            melhor.innerText = 'aplicativos.html'
+        } else if (age > 18 && age < 35) {
+            melhor.innerText = 'dicas.html'
+        } else if (age > 35) {
+            melhor.innerText = 'importantes.html'
+        }
 }

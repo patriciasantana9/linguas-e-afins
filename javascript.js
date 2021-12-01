6299ec856e64ee81a08734451ff581b8cb44c6d5
Patrícia Santana <patriciasan3@gmail.com>
2021-11-30 21:11:52 -0300

    modificações na data

--- javascript.js
+++ javascript.js
@@ -6,18 +6,94 @@ function hora() {
     let minutos = horas.getMinutes()
 
     let dia = new Date()
-    let diasem = dia.getDay() //como diabos colocar o dia em nome, não número?
+    let diasem = dia.getDay() 
 
- /*   switch (key) {
-        case value:
-            
+    let data = new Date()
+    let datasem = data.getDate()
+
+    let mesano = new Date()
+    let mes = mesano.getMonth()  
+
+    //daho.innerText = `${diasem}, ${datahora}:${minutos}`
+
+    switch (diasem) {
+        case 0:
+            daho.innerText = `${datahora}:${minutos}, domingo, ${datasem} de`
             break;
     
+         case 1:
+            daho.innerText =`${datahora}:${minutos}, segunda-feira, ${datasem} de `
+            break;
+
+         case 2:
+            daho.innerText = `${datahora}:${minutos}, terça-feira, ${datasem} de `
+            break;
+
+         case 3:
+            daho.innerText = `${datahora}:${minutos}, quarta-feira, ${datasem} de `
+            break;
+
+         case 4:
+            daho.innerText = `${datahora}:${minutos}, quinta-feira, ${datasem} de `
+            break;
+
+         case 5:
+            daho.innerText = `${datahora}:${minutos}, sexta-feira, ${datasem} de `
+            break;
+
+         case 6:
+            daho.innerText = `${datahora}:${minutos}, sábado, ${datasem} de `
+            break;
+
+        default:
+            daho.innerText = '[ERRO]'
+            break;
+    }
+
+    switch (mes) {
+        case 0:
+            daho.innerText += ` janeiro`
+            break;
+        case 1:
+            daho.innerText += ` fevereiro`
+            break;
+        case 2:
+            daho.innerText += ` março`
+            break;
+        case 3:
+            daho.innerText += ` abril`
+            break;
+        case 4:
+            daho.innerText += ` maio`
+            break;
+        case 5:
+            daho.innerText += ` junho`
+            break;
+        case 6:
+            daho.innerText += ` julho`
+            break;
+        case 7:
+            daho.innerText += ` agosto`
+            break;
+        case 8:
+            daho.innerText += ` setembro`
+            break;
+        case 9:
+            daho.innerText += ` outubro`
+            break;
+        case 10:
+            daho.innerText += ` novembro`
+            break;
+        case 11:
+            daho.innerText += ` dezembro`
+            break;
+
         default:
             break;
     }
-    daho.innerText = `${diasem}, ${datahora}:${minutos}`
-}*/
+    
+    
+}
 
 function best() {    
     var age = Number(idade.value)

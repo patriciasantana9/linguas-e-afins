347428d3bc45bba63d80114cb163929004d85846
Patrícia Santana <patriciasan3@gmail.com>
2021-12-03 12:10:58 -0300

    addEventLi

--- javascript.js
+++ javascript.js
@@ -1,100 +1,102 @@
-function hora() {
-    let daho = document.querySelector('div#daho')
-    
-    let horas = new Date()
-    let datahora = horas.getHours()
-    let minutos = horas.getMinutes()
+window.addEventListener('load', hora)     //fazer o carregamento ser automático, então tirar o código do HTML
 
-    let dia = new Date()
-    let diasem = dia.getDay() 
+    function hora() {
+        let daho = document.querySelector('div#daho')
+        
+        let horas = new Date()
+        let datahora = horas.getHours()
+        let minutos = horas.getMinutes()
 
-    let data = new Date()
-    let datasem = data.getDate()
+        let dia = new Date()
+        let diasem = dia.getDay() 
 
-    let mesano = new Date()
-    let mes = mesano.getMonth()  
+        let data = new Date()
+        let datasem = data.getDate()
 
-    //daho.innerText = `${diasem}, ${datahora}:${minutos}`
+        let mesano = new Date()
+        let mes = mesano.getMonth()  
 
-    
-    switch (diasem) {
-        case 0:
-            daho.innerText = `${datahora}:${minutos}, domingo, ${datasem} de`
-            break;
-    
-         case 1:
-            daho.innerText =`${datahora}:${minutos}, segunda-feira, ${datasem} de `
-            break;
+        //daho.innerText = `${diasem}, ${datahora}:${minutos}`
 
-         case 2:
-            daho.innerText = `${datahora}:${minutos}, terça-feira, ${datasem} de `
-            break;
+        
+        switch (diasem) {
+            case 0:
+                daho.innerText = `${datahora}:${minutos}, domingo, ${datasem} de`
+                break;
+        
+            case 1:
+                daho.innerText =`${datahora}:${minutos}, segunda-feira, ${datasem} de `
+                break;
 
-         case 3:
-            daho.innerText = `${datahora}:${minutos}, quarta-feira, ${datasem} de `
-            break;
+            case 2:
+                daho.innerText = `${datahora}:${minutos}, terça-feira, ${datasem} de `
+                break;
 
-         case 4:
-            daho.innerText = `${datahora}:${minutos}, quinta-feira, ${datasem} de `
-            break;
+            case 3:
+                daho.innerText = `${datahora}:${minutos}, quarta-feira, ${datasem} de `
+                break;
 
-         case 5:
-            daho.innerText = `${datahora}:${minutos}, sexta-feira, ${datasem} de `
-            break;
+            case 4:
+                daho.innerText = `${datahora}:${minutos}, quinta-feira, ${datasem} de `
+                break;
 
-         case 6:
-            daho.innerText = `${datahora}:${minutos}, sábado, ${datasem} de `
-            break;
+            case 5:
+                daho.innerText = `${datahora}:${minutos}, sexta-feira, ${datasem} de `
+                break;
 
-        default:
-            daho.innerText = '[ERRO]'
-            break;
+            case 6:
+                daho.innerText = `${datahora}:${minutos}, sábado, ${datasem} de `
+                break;
+
+            default:
+                daho.innerText = '[ERRO]'
+                break;
+        }
+
+        switch (mes) {
+            case 0:
+                daho.innerText += ` janeiro`
+                break;
+            case 1:
+                daho.innerText += ` fevereiro`
+                break;
+            case 2:
+                daho.innerText += ` março`
+                break;
+            case 3:
+                daho.innerText += ` abril`
+                break;
+            case 4:
+                daho.innerText += ` maio`
+                break;
+            case 5:
+                daho.innerText += ` junho`
+                break;
+            case 6:
+                daho.innerText += ` julho`
+                break;
+            case 7:
+                daho.innerText += ` agosto`
+                break;
+            case 8:
+                daho.innerText += ` setembro`
+                break;
+            case 9:
+                daho.innerText += ` outubro`
+                break;
+            case 10:
+                daho.innerText += ` novembro`
+                break;
+            case 11:
+                daho.innerText += ` dezembro`
+                break;
+
+            default:
+                break;
+        }
+        
+        
     }
-
-    switch (mes) {
-        case 0:
-            daho.innerText += ` janeiro`
-            break;
-        case 1:
-            daho.innerText += ` fevereiro`
-            break;
-        case 2:
-            daho.innerText += ` março`
-            break;
-        case 3:
-            daho.innerText += ` abril`
-            break;
-        case 4:
-            daho.innerText += ` maio`
-            break;
-        case 5:
-            daho.innerText += ` junho`
-            break;
-        case 6:
-            daho.innerText += ` julho`
-            break;
-        case 7:
-            daho.innerText += ` agosto`
-            break;
-        case 8:
-            daho.innerText += ` setembro`
-            break;
-        case 9:
-            daho.innerText += ` outubro`
-            break;
-        case 10:
-            daho.innerText += ` novembro`
-            break;
-        case 11:
-            daho.innerText += ` dezembro`
-            break;
-
-        default:
-            break;
-    }
-    
-    
-}
 
 /*function best() {    
     var age = Number(idade.value)
